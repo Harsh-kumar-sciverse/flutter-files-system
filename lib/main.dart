f8b0c4b9bf001baa329146a332105bca2a9d0d48
Harsh <harsh.kumar@sciverse.co.in>
2023-07-06 11:16:20 +0530

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StreamList(),
+      home: const MyHomePage(),
     );
   }
 }
